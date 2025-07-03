1ffb79227fdf05399d4b070b99c81e287e29c516
pablo <pablo.lima@solutis.com.br>
2025-07-03 19:16:21 -0300

    feat: documentação do swagger

--- src/main/java/com/hubsi/authmicroservice/AuthmicroserviceApplication.java
+++ src/main/java/com/hubsi/authmicroservice/AuthmicroserviceApplication.java
@@ -1,9 +1,18 @@
 package com.hubsi.authmicroservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Minha API",
+				version = "1.0",
+				description = "Documentação da API com SpringDoc"
+		)
+)
 public class AuthmicroserviceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/hubsi/authmicroservice/controller/AuthController.java
+++ src/main/java/com/hubsi/authmicroservice/controller/AuthController.java
@@ -6,6 +6,11 @@ import com.hubsi.authmicroservice.dto.response.AuthResponse;
 import com.hubsi.authmicroservice.dto.response.RegisterResponse;
 import com.hubsi.authmicroservice.services.AuthService;
 import com.hubsi.authmicroservice.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +26,12 @@ public class AuthController {
     private final UserService userService;
     private final AuthService authService;
 
+    @Operation(summary = "Registrar um novo usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário registrado com sucesso",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = RegisterResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos", content = @Content)
+    })
     @PostMapping("/register")
     public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(userService.registerUser(request));
